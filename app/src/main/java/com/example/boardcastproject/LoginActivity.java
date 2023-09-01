a28ceb90f93c14bd2f55f6c4dda1adea52eb4f01
Liao Yin <1691156671@qq.com>
2023-09-01 19:32:20 +0800

    first commit

---
+++ app/src/main/java/com/example/boardcastproject/LoginActivity.java
@@ -0,0 +1,40 @@
+package com.example.boardcastproject;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import android.content.Intent;
+public class LoginActivity extends BaseActivity {
+    private EditText accountEdit;
+
+    private EditText passwordEdit;
+
+    private Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        accountEdit = (EditText) findViewById(R.id.account);
+        passwordEdit = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.login);
+//        login.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String account = accountEdit.getText().toString();
+//                String password = passwordEdit.getText().toString();
+//                // 如果账号是admin且密码是123456，就认为登录成功
+//                if (account.equals("admin") && password.equals("123456")) {
+//                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+//                    startActivity(intent);
+//                    finish();
+//                } else {
+//                    Toast.makeText(LoginActivity.this, "账号或密码错误,请重新输入账号密码",
+//                            Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        });
+    }
+}
